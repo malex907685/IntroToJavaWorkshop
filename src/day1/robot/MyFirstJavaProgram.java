90b6efbbf950c4efd8ef06dbb3888f07d794febd
Garrett-Thomas <league@WTS10.attlocal.net>
2017-07-18 14:55:40 -0700

    learn java robot

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -5,11 +5,22 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
+
 		// START HERE
 
-	
-		
-		
+		Robot bob = new Robot();
+		for (int i = 0; i < 4; i++) {
+			bob.setSpeed(6);
+			bob.penDown();
+			bob.move(80);
+
+			bob.turn(-140);
+			bob.move(100);
+			bob.turn(-140);
+			bob.move(100);
+			bob.turn(-140);
+			bob.move(100);
+
+		}
 	}
 }
